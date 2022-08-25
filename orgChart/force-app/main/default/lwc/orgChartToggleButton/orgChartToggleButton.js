4869a70c2f180f885cdee4c1a2435c6e2017c327
SIBIL JOE KURIAN <sikurian@deloitte.ca>
2022-08-25 11:13:14 -0400

    apex code and lwc

--- orgChart/force-app/main/default/lwc/orgChartToggleButton/orgChartToggleButton.js
+++ orgChart/force-app/main/default/lwc/orgChartToggleButton/orgChartToggleButton.js
@@ -2,13 +2,16 @@ import { LightningElement,api } from 'lwc';
 
 export default class OrgChartToggleButton extends LightningElement {
     @api level;
-    toggle = false;
-
+    @api toggle = false;
+    @api count;
     handleOnClickToggle(){
         this.toggle?this.toggle=false:this.toggle=true;
         const selectedEvent = new CustomEvent("clickhanlder", {
             detail: this.toggle
         });
         this.dispatchEvent(selectedEvent);
+    }
+    get getToggle(){
+        return this.toggle;
     }
 }
