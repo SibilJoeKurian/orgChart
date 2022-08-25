4269fcffb822057151c04cda50f3113b06d98906
SIBIL JOE KURIAN <sikurian@deloitte.ca>
2022-08-25 11:41:02 -0400

    refactored

--- orgChart/force-app/main/default/lwc/orgChartLightningCard/orgChartLightningCard.js
+++ orgChart/force-app/main/default/lwc/orgChartLightningCard/orgChartLightningCard.js
@@ -8,10 +8,15 @@ export default class OrgChartLightningCard extends NavigationMixin(LightningElem
     @api hireDate;
     @api birthMonth;
     @api birthDay;
-    
+    @api photo;
+    rendercallback(){
+        console.log('::: '+this.photo)
+    }
     viewRecord(event) {
         let url = window.location.origin +'/'+event.currentTarget.dataset.id;
         window.open(url,'_blank').focus();
     }
-
+    get getPhoto(){
+        return this.photo;
+    }
 }
